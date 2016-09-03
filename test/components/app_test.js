5edf080634803e1f71a743e476657f89110520b5
Santosh Gautam <santoshgautam7@gmail.com>
2016-09-03 16:15:56 -0700

    Initial test setup.

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,9 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+describe
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+it
+
+expect
